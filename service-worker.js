ceea52bc20ca7f55c600cb4ccdad8fe55e755306
BHUSHAN112 <bhushangandhi1123@gmail.com>
2025-08-08 16:04:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,42 @@
-const CACHE_NAME = 'cpr-sensor-cache-v1';
+const CACHE_NAME = 'cpr-training-app-v2';
 const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json',
-  'icon.png'
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './dial.js',
+  './depth-bar.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
